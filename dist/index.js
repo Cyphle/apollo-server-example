b93fafc6d2c6cef6c87974fb670e12cb57c768ec
Cyphle <cyril.phamle@gmail.com>
2023-10-26 15:38:06 +0200

    first commit

---
+++ dist/index.js
@@ -0,0 +1,50 @@
+import { ApolloServer } from '@apollo/server';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { expressMiddleware } from '@apollo/server/express4';
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+};
+const books = [
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin',
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster',
+    },
+];
+const app = express();
+const httpServer = http.createServer(app);
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+app.use('/graphql', cors(), express.json(), expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+}));
+await server.start();
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
---
+++ src/index.ts
@@ -0,0 +1,83 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { expressMiddleware } from '@apollo/server/express4';
+
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+// // The ApolloServer constructor requires two parameters: your schema
+// // definition and your set of resolvers.
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+//
+//
+// // Passing an ApolloServer instance to the `startStandaloneServer` function:
+// //  1. creates an Express app
+// //  2. installs your ApolloServer instance as middleware
+// //  3. prepares your app to handle incoming requests
+// const { url } = await startStandaloneServer(server, {
+//   listen: { port: 4000 },
+// });
+//
+// console.log(`🚀  Server ready at: ${url}`);
+
+interface MyContext {
+  token?: String;
+}
+
+const app = express();
+const httpServer = http.createServer(app);
+const server = new ApolloServer<MyContext>({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+await server.start();
+app.use(
+    '/graphql',
+    cors<cors.CorsRequest>(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+    }),
+);
+
+await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
