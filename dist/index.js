dd2bad9941dbb07bc96a50321aa6691512eea16c
Cyphle <cyril.phamle@gmail.com>
2023-10-26 17:13:29 +0200

    Add type generator

--- dist/index.js
+++ dist/index.js
@@ -4,22 +4,29 @@ import http from 'http';
 import cors from 'cors';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import { expressMiddleware } from '@apollo/server/express4';
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
+import { readFileSync } from 'fs';
+const typeDefs = readFileSync('./src/schemas/schema.graphql', { encoding: 'utf-8' });
+//
+// const typeDefs = `#graphql
+//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+//
+//   # This "Book" type defines the queryable fields for every book in our data source."
+//   type Book {
+//     """
+//     Description for field
+//     Supports **multi-line** description for your [API](http://example.com)!
+//     """
+//     title: String
+//     author: String
+//   }
+//
+//   # The "Query" type is special: it lists all of the available queries that
+//   # clients can execute, along with the return type for each. In this
+//   # case, the "books" query returns an array of zero or more Books (defined above).
+//   type Query {
+//     books: [Book]
+//   }
+// `;
 const resolvers = {
     Query: {
         books: () => books,
@@ -42,9 +49,9 @@ const server = new ApolloServer({
     resolvers,
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
+await server.start();
 app.use('/graphql', cors(), express.json(), expressMiddleware(server, {
     context: async ({ req }) => ({ token: req.headers.token }),
 }));
-await server.start();
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
 console.log(`🚀 Server ready at http://localhost:4000/graphql`);
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,52 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import { expressMiddleware } from '@apollo/server/express4';
+import { readFileSync } from 'fs';
+import { Book, Resolvers } from './generated/graphql';
 
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+const typeDefs = readFileSync('./src/schemas/schema.graphql', { encoding: 'utf-8' });
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
+//
+// const typeDefs = `#graphql
+//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+//
+//   # This "Book" type defines the queryable fields for every book in our data source."
+//   type Book {
+//     """
+//     Description for field
+//     Supports **multi-line** description for your [API](http://example.com)!
+//     """
+//     title: String
+//     author: String
+//   }
+//
+//   # The "Query" type is special: it lists all of the available queries that
+//   # clients can execute, along with the return type for each. In this
+//   # case, the "books" query returns an array of zero or more Books (defined above).
+//   type Query {
+//     books: [Book]
+//   }
+// `;
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
+// const resolvers = {
+//   Query: {
+//     books: () => books,
+//   },
+// };
 
-const resolvers = {
+export const resolvers: Resolvers = {
   Query: {
-    books: () => books,
+    // TypeScript now complains about the below resolver because
+    // the data returned by this resolver doesn't match the schema type
+    // (i.e., type Query { books: [Book] })
+    books: (): Book[] => {
+      return books;
+    },
   },
-};
+}
 
 const books = [
   {
