7f0128f9c689988bd1e020be3b359b094ff8905a
Cyphle <cyril.phamle@gmail.com>
2023-10-27 20:19:07 +0200

    split schema

--- src/codegen.ts
+++ src/codegen.ts
@@ -3,7 +3,10 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "./src/schemas/schema.graphql",
+  schema: [
+    "./src/schemas/schema.graphql",
+    "./src/schemas/book.graphql"
+    ],
   generates: {
     "src/generated/graphql.ts": {
       plugins: ["typescript", "typescript-resolvers"]
